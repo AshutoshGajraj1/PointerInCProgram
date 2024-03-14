7727cd9e364b3e25628361cf8b8d40ac0c31167e
AshutoshGajraj1 <gajrajashutosh1@gmail.com>
2024-03-14 12:28:11 +0545

    first commit

---
+++ pointer.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int a[5];
+
+    for(int i = 0; i< 5; ++i){
+        printf("Enter a number: ");
+        scanf("%d", a+i);
+    }
+     
+    printf("The array is :\n");
+    for(int i = 0; i<5; ++i){
+        printf("%d\t", a[i]);
+    }
+    return 0;
+}
